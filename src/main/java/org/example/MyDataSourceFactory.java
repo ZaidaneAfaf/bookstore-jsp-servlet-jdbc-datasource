d0d7a7c007af8eaad9f7247dd26bb6bd1b7c9e8b
Afaf z <afafz@gmail.com>
2025-10-14 22:08:08 +0200

    fix: Correction de 5 issues SonarQube (ajout @Override et attributs lang/for)

--- src/main/java/org/example/MyDataSourceFactory.java
+++ src/main/java/org/example/MyDataSourceFactory.java
@@ -1,65 +1,34 @@
 package org.example;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
-import org.apache.tomcat.jdbc.pool.PoolProperties;
+import org.apache.commons.dbcp2.BasicDataSource;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class MyDataSourceFactory {
     
-    private static DataSource dataSource;
-    
     // Constructeur privé pour empêcher l'instanciation
     private MyDataSourceFactory() {
         throw new UnsupportedOperationException("Utility class");
     }
     
-    public static synchronized DataSource getDataSource() {
-        if (dataSource == null) {
-            Properties props = new Properties();
-            try (InputStream input = MyDataSourceFactory.class.getClassLoader()
-                    .getResourceAsStream("db.properties")) {
-                if (input == null) {
-                    throw new IOException("Unable to find db.properties");
-                }
-                props.load(input);
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new RuntimeException("Failed to load database properties", e);
-            }
-            
-            PoolProperties p = new PoolProperties();
-            p.setUrl(props.getProperty("db.url"));
-            p.setDriverClassName(props.getProperty("db.driver"));
-            p.setUsername(props.getProperty("db.username"));
-            p.setPassword(props.getProperty("db.password"));
-            p.setJmxEnabled(true);
-            p.setTestWhileIdle(false);
-            p.setTestOnBorrow(true);
-            p.setValidationQuery("SELECT 1");
-            p.setTestOnReturn(false);
-            p.setValidationInterval(30000);
-            p.setTimeBetweenEvictionRunsMillis(30000);
-            p.setMaxActive(100);
-            p.setInitialSize(10);
-            p.setMaxWait(10000);
-            p.setRemoveAbandonedTimeout(60);
-            p.setMinEvictableIdleTimeMillis(30000);
-            p.setMinIdle(10);
-            p.setLogAbandoned(true);
-            p.setRemoveAbandoned(true);
-            
-            dataSource = new DataSource();
-            dataSource.setPoolProperties(p);
+    public static DataSource getDataSource() {
+        Properties prop = new Properties();
+        try (InputStream input = MyDataSourceFactory.class.getClassLoader().getResourceAsStream("db.properties")) {
+            prop.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
         }
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(prop.getProperty("db.driver"));
+        dataSource.setUrl(prop.getProperty("db.url"));
+        dataSource.setUsername(prop.getProperty("db.username"));
+        dataSource.setPassword(prop.getProperty("db.password"));
+        dataSource.setInitialSize(5); // Initial number of connections
+        dataSource.setMaxTotal(10); // Maximum number of connections
         return dataSource;
-    }
-    
-    public static synchronized void closeDataSource() {
-        if (dataSource != null) {
-            dataSource.close();
-        }
     }
 }
