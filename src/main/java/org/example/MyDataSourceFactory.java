c4ffd0b21df094050215bc64f5d829485484dcae
Afaf z <afafz@gmail.com>
2025-10-14 22:06:04 +0200

    fix: Correction de 5 issues SonarQube (ajout @Override et attributs lang/for)

--- src/main/java/org/example/MyDataSourceFactory.java
+++ src/main/java/org/example/MyDataSourceFactory.java
@@ -1,16 +1,15 @@
 package org.example;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
 
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class MyDataSourceFactory {
     
-    private static HikariDataSource dataSource;
+    private static DataSource dataSource;
     
     // Constructeur privé pour empêcher l'instanciation
     private MyDataSourceFactory() {
@@ -31,24 +30,35 @@ public class MyDataSourceFactory {
                 throw new RuntimeException("Failed to load database properties", e);
             }
             
-            HikariConfig config = new HikariConfig();
-            config.setJdbcUrl(props.getProperty("db.url"));
-            config.setUsername(props.getProperty("db.username"));
-            config.setPassword(props.getProperty("db.password"));
-            config.setDriverClassName(props.getProperty("db.driver"));
-            config.setMaximumPoolSize(10);
-            config.setMinimumIdle(5);
-            config.setConnectionTimeout(30000);
-            config.setIdleTimeout(600000);
-            config.setMaxLifetime(1800000);
+            PoolProperties p = new PoolProperties();
+            p.setUrl(props.getProperty("db.url"));
+            p.setDriverClassName(props.getProperty("db.driver"));
+            p.setUsername(props.getProperty("db.username"));
+            p.setPassword(props.getProperty("db.password"));
+            p.setJmxEnabled(true);
+            p.setTestWhileIdle(false);
+            p.setTestOnBorrow(true);
+            p.setValidationQuery("SELECT 1");
+            p.setTestOnReturn(false);
+            p.setValidationInterval(30000);
+            p.setTimeBetweenEvictionRunsMillis(30000);
+            p.setMaxActive(100);
+            p.setInitialSize(10);
+            p.setMaxWait(10000);
+            p.setRemoveAbandonedTimeout(60);
+            p.setMinEvictableIdleTimeMillis(30000);
+            p.setMinIdle(10);
+            p.setLogAbandoned(true);
+            p.setRemoveAbandoned(true);
             
-            dataSource = new HikariDataSource(config);
+            dataSource = new DataSource();
+            dataSource.setPoolProperties(p);
         }
         return dataSource;
     }
     
     public static synchronized void closeDataSource() {
-        if (dataSource != null && !dataSource.isClosed()) {
+        if (dataSource != null) {
             dataSource.close();
         }
     }
