9d8e958b4c12e953d6ea2919c31ab12497023f49
Afaf z <afafz@gmail.com>
2025-10-14 22:34:14 +0200

    fix: Correction de 5 issues SonarQube (ajout @Override et attributs lang/for)

---
+++ src/test/java/org/example/BookTest.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BookTest {
+    
+    @Test
+    public void testBookGettersAndSetters() {
+        Book book = new Book();
+        
+        book.setId(1L);
+        book.setTitle("Test Book");
+        book.setAuthor("Test Author");
+        
+        assertEquals(Long.valueOf(1L), book.getId());
+        assertEquals("Test Book", book.getTitle());
+        assertEquals("Test Author", book.getAuthor());
+    }
+    
+    @Test
+    public void testBookCreation() {
+        Book book = new Book();
+        assertNotNull(book);
+    }
+    
+    @Test
+    public void testBookTitle() {
+        Book book = new Book();
+        book.setTitle("Sample Title");
+        assertNotNull(book.getTitle());
+        assertEquals("Sample Title", book.getTitle());
+    }
+    
+    @Test
+    public void testBookAuthor() {
+        Book book = new Book();
+        book.setAuthor("Sample Author");
+        assertNotNull(book.getAuthor());
+        assertEquals("Sample Author", book.getAuthor());
+    }
+    
+    @Test
+    public void testBookId() {
+        Book book = new Book();
+        book.setId(100L);
+        assertNotNull(book.getId());
+        assertEquals(Long.valueOf(100L), book.getId());
+    }
+}
